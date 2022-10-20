12b731874e4d27ccea311df1d6cf235e4a934474
Younghee <kwak.younghee@gmail.com>
2022-10-20 11:52:35 -0700

    define a Planet class & create a list of Planet instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,21 @@
 from flask import Blueprint
 
+class Planet():
+    def __init__(self, id, name, description, num_satellite):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.num_satellite = num_satellite
+
+planets = [
+    Planet(1, "Mercury", "mostly of rock", 0),
+    Planet(2, "Venus", "temperature is higher than 850F", 0),
+    Planet(3, "Earth", "our planet", 1),
+    Planet(4, "Mars", "red planet", 2),
+    Planet(5, "Jupiter", "gas giant planet", 63),
+    Planet(6, "Saturn", "most colorful planet", 56),
+    Planet(7, "Uranus", "night and day lasts 42 years", 21),
+    Planet(8, "Neptune", "most distant planet", 13)
+]
+
+
