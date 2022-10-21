fed41a99214555bbd0336b6678d22e75be1b19c6
Melissa <ma.pena94@gmail.com>
2022-10-21 14:29:28 -0700

    Wave #2 completed. Add the function get_one_planet & handeling 404 and 400 errors

--- app/routes.py
+++ app/routes.py
@@ -18,15 +18,38 @@ planets = [
     Planet(8, "Neptune", "most distant planet", 13)
 ]
 
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+planets_bp = Blueprint("planets", __name__,url_prefix="/planets")
 @planets_bp.route('', methods=['GET'])
 def get_all_planets():
     result = []
     for planet in planets:
         planet_dict = {"id":planet.id, "name":planet.name,
-                       "description":planet.description, 
-                       "num_satellite":planet.num_satellite}
+                    "description":planet.description, 
+                    "num_satellite":planet.num_satellite}
         result.append(planet_dict)
 
     return jsonify(result), 200
 
+@planets_bp.route('/<planet_id>', methods = ['GET'])
+def get_one_planet(planet_id):
+
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return jsonify({"msg": f"invalid data type: {planet_id}"}),400
+        
+    chosen_planet = None
+    for planet in planets:
+        if planet.id == planet_id:
+            chosen_planet = planet 
+    if chosen_planet is None:
+        return jsonify({"msg": f"Could not find the planet with the id: {planet_id}"}), 404
+
+    return_chosen_planet = {
+        "id": chosen_planet.id,
+        "name": chosen_planet.name,
+        "description": chosen_planet.description,
+        "num_satellite": chosen_planet.num_satellite
+    }
+
+    return jsonify(return_chosen_planet), 200
