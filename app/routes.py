b38d6ac221bb8868faad94f7f092e4b12e324415
Melissa <ma.pena94@gmail.com>
2022-10-27 14:36:07 -0700

    Create a POST request and modified get_all_planets

--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,73 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app import db
+from app.models.planet import Planet
 
-class Planet():
-    def __init__(self, id, name, description, num_satellite):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_satellite = num_satellite
+# class Planet():
+#     def __init__(self, id, name, description, num_satellite):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.num_satellite = num_satellite
 
-planets = [
-    Planet(1, "Mercury", "mostly of rock", 0),
-    Planet(2, "Venus", "temperature is higher than 850F", 0),
-    Planet(3, "Earth", "our planet", 1),
-    Planet(4, "Mars", "red planet", 2),
-    Planet(5, "Jupiter", "gas giant planet", 63),
-    Planet(6, "Saturn", "most colorful planet", 56),
-    Planet(7, "Uranus", "night and day lasts 42 years", 21),
-    Planet(8, "Neptune", "most distant planet", 13)
-]
+# planets = [
+#     Planet(1, "Mercury", "mostly of rock", 0),
+#     Planet(2, "Venus", "temperature is higher than 850F", 0),
+#     Planet(3, "Earth", "our planet", 1),
+#     Planet(4, "Mars", "red planet", 2),
+#     Planet(5, "Jupiter", "gas giant planet", 63),
+#     Planet(6, "Saturn", "most colorful planet", 56),
+#     Planet(7, "Uranus", "night and day lasts 42 years", 21),
+#     Planet(8, "Neptune", "most distant planet", 13)
+# ]
 
 planets_bp = Blueprint("planets", __name__,url_prefix="/planets")
 @planets_bp.route('', methods=['GET'])
 def get_all_planets():
     result = []
-    for planet in planets:
+    all_planets= Planet.query.all()
+    for planet in all_planets:
         planet_dict = {"id":planet.id, "name":planet.name,
                     "description":planet.description, 
-                    "num_satellite":planet.num_satellite}
+                    "num_moon":planet.num_moon}
         result.append(planet_dict)
 
     return jsonify(result), 200
 
-@planets_bp.route('/<planet_id>', methods = ['GET'])
-def get_one_planet(planet_id):
+# @planets_bp.route('/<planet_id>', methods = ['GET'])
+# def get_one_planet(planet_id):
 
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        return jsonify({"msg": f"invalid data type: {planet_id}"}),400
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         return jsonify({"msg": f"invalid data type: {planet_id}"}),400
         
-    chosen_planet = None
-    for planet in planets:
-        if planet.id == planet_id:
-            chosen_planet = planet 
-    if chosen_planet is None:
-        return jsonify({"msg": f"Could not find the planet with the id: {planet_id}"}), 404
+#     chosen_planet = None
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             chosen_planet = planet 
+#     if chosen_planet is None:
+#         return jsonify({"msg": f"Could not find the planet with the id: {planet_id}"}), 404
 
-    return_chosen_planet = {
-        "id": chosen_planet.id,
-        "name": chosen_planet.name,
-        "description": chosen_planet.description,
-        "num_satellite": chosen_planet.num_satellite
-    }
+#     return_chosen_planet = {
+#         "id": chosen_planet.id,
+#         "name": chosen_planet.name,
+#         "description": chosen_planet.description,
+#         "num_moon": chosen_planet.num_moon
+#     }
 
-    return jsonify(return_chosen_planet), 200
+#     return jsonify(return_chosen_planet), 200
+
+@planets_bp.route('', methods=['POST'])
+def create_one_planet():
+    request_body = request.get_json()
+
+    new_planet = Planet(name= request_body['name'], 
+                        description=request_body['description'],
+                        num_moon=request_body['num_moon']
+                        )
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify({'msg': f"Successfully created Planet with id= {new_planet.id}"}, 201)
+    
