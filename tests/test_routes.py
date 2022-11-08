b030fe59bbd15a524f4b477d1653cd551d46de43
Melissa <ma.pena94@gmail.com>
2022-11-08 11:12:30 -0800

    test get one planet still working

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,3 +18,10 @@ def test_get_one_planet_returns_json(client, two_planets):
         "description": "Red Planet",
         "num_moon": 2
     } 
+
+def test_get_one_planet_empty_db_return_404(client):
+    response = client.get("planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert "msg" in response_body
